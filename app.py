da89551d41e08222e09b324a608be163f49757bd
amir2c <amirhosein.toosi@gmail.com>
2025-07-17 15:04:05 +0330

    old style function separated

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
-# import docx
-# import spire.doc
 import docx2txt2
 from bs4 import BeautifulSoup
 from markdown import markdown
 import re
+
+
 
 def markdown_to_text(markdown_string):
     """ Converts a markdown string to plaintext """
@@ -32,10 +32,49 @@ def clean_text_from_raw_pdf(rawtext):
     cleaned_text = re.sub(r'\s+', ' ', rawtext)
     # Remove special characters or symbols (customize as needed)
     # This example keeps alphanumeric characters, spaces, periods, and commas
-    cleaned_text = re.sub(r'[^a-zA-Z0-9\s.,/@]', ' ', cleaned_text)
+    cleaned_text = re.sub(r'[^\u0600-\u06FF\uFB50-\uFDFF\uFE70-\uFEFFa-zA-Z0-9\s.,/@]', ' ', cleaned_text)
     # Remove leading/trailing whitespace
     cleaned_text = cleaned_text.strip()
     return cleaned_text
+
+
+def oldstyle_reader(uploaded_files):
+    extracted_texts = list()
+    for uploaded_file in uploaded_files:
+
+        text = ""
+        if uploaded_file.name.endswith('.pdf'):
+            pdf_reader = PdfReader(uploaded_file)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            # print(f"/n/n PDF TEXT BEFORE CLEANING: {text}")
+            text = clean_text_from_raw_pdf(text)
+            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
+            extracted_texts.append(text)
+
+        elif uploaded_file.name.endswith('.txt'):
+            text = uploaded_file.read().decode("utf-8")
+            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
+            extracted_texts.append(text)
+
+        elif uploaded_file.name.endswith('.docx') :
+            text = docx2txt2.extract_text(uploaded_file)
+            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
+            extracted_texts.append(text)
+            
+        elif uploaded_file.name.endswith('.md'):
+            text = uploaded_file.read().decode("utf-8")
+            text = markdown_to_text(text)
+            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
+            extracted_texts.append(text)
+
+        else:
+            st.error(f"Unsupported file type: {uploaded_file.name}. Please upload a PDF, TXT, DOCX, or MD file.")
+    
+    if len(extracted_texts) > 0:
+        return extracted_texts
+    elif len(uploaded_files) > 0:
+        st.error(f"Unknown error!! Couldn't extract any text from uploaded documents.")
 
 
 def main():
@@ -50,58 +89,8 @@ def main():
 
     uploaded_files = st.file_uploader("Please upload your resume files here!", accept_multiple_files=True)
 
-    for uploaded_file in uploaded_files:
-        # st.write("filename:", uploaded_file.name)
-
-        if uploaded_file.name.endswith('.pdf'):
-            pdf_reader = PdfReader(uploaded_file)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            print(f"/n/n PDF TEXT BEFORE CLEANING: {text}")
-            text = clean_text_from_raw_pdf(text)
-            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
-
-        elif uploaded_file.name.endswith('.txt'):
-            text = ""
-            text = uploaded_file.read().decode("utf-8")
-            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
-
-        elif uploaded_file.name.endswith('.docx'):
-            # doc = docx.Document(uploaded_file)
-            # text = "\n".join([para.text for para in doc.paragraphs])
-            # st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
-
-            # full_text = []
-            # document = docx.Document(uploaded_file)
-            # for paragraph in document.paragraphs:
-            #     full_text.append(paragraph.text)
-            # st.text_area(f"Content of Resume file {uploaded_file.name}", value=full_text, height=700)
-            # print(f"/n/n {full_text} ")
-
-            # # Create a Document object
-            # document = spire.doc.Document()
-            # # Load a Word document
-            # document.LoadFromFile("Sample.docx")
-
-            # # Extract the text of the document
-            # document_text = document.GetText()
-
-            # st.text_area(f"Content of Resume file {uploaded_file.name}", value=document_text, height=700)
-            # print(f"/n/n {document_text} ")
-            # document.Close()
-
-            document_text = docx2txt2.extract_text(uploaded_file)
-            st.text_area(f"Content of Resume file {uploaded_file.name}", value=document_text, height=700)
-            
-
-        elif uploaded_file.name.endswith('.md'):
-            text = uploaded_file.read().decode("utf-8")
-            text = markdown_to_text(text)
-            st.text_area(f"Content of Resume file {uploaded_file.name}", value=text, height=700)
-
-        else:
-            st.error(f"Unsupported file type: {uploaded_file.name}. Please upload a PDF, TXT, DOCX, or MD file.")
+    oldstyle_reader(uploaded_files)
+    
 
 
 
